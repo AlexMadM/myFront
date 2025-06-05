e21139d1a29ac62b03cd2789ef45757d6b37a518
AlexMadM <alex.alexandrov.1988@gmail.com>
2025-06-05 21:54:58 +0300

    add profile

--- src/features/AuthByUsername/model/selectors/getLoginIsLoading/getLoginIsLoading.test.ts
+++ src/features/AuthByUsername/model/selectors/getLoginIsLoading/getLoginIsLoading.test.ts
@@ -1,23 +1,16 @@
-import { DeepPartial } from '@reduxjs/toolkit';
 import { StateSchema } from 'app/providers/StoreProvider';
 import { getLoginIsLoading } from './getLoginIsLoading';
 
 describe('getLoginIsLoading.test', () => {
-    test('should return true if isLoading is true', () => {
+    test('should return true', () => {
         const state: DeepPartial<StateSchema> = {
-            loginForm: { isLoading: true },
+            loginForm: {
+                isLoading: true,
+            },
         };
         expect(getLoginIsLoading(state as StateSchema)).toEqual(true);
     });
-
-    test('should return false if isLoading is false', () => {
-        const state: DeepPartial<StateSchema> = {
-            loginForm: { isLoading: false },
-        };
-        expect(getLoginIsLoading(state as StateSchema)).toEqual(false);
-    });
-
-    test('should return false if loginForm is undefined', () => {
+    test('should work with empty state', () => {
         const state: DeepPartial<StateSchema> = {};
         expect(getLoginIsLoading(state as StateSchema)).toEqual(false);
     });
--- src/shared/lib/tests/TestAsyncThunk/TestAsyncThunk.ts
+++ src/shared/lib/tests/TestAsyncThunk/TestAsyncThunk.ts
@@ -1,30 +1,31 @@
-import { AsyncThunkAction, DeepPartial } from '@reduxjs/toolkit';
 import { StateSchema } from 'app/providers/StoreProvider';
+import { AsyncThunkAction } from '@reduxjs/toolkit';
 import axios, { AxiosStatic } from 'axios';
+
+type ActionCreatorType<Return, Arg, RejectedValue>
+    = (arg: Arg) => AsyncThunkAction<Return, Arg, { rejectValue: RejectedValue }>;
 
 jest.mock('axios');
 
-const mockedAxios = jest.mocked(axios);
+const mockedAxios = jest.mocked(axios, true);
 
 export class TestAsyncThunk<Return, Arg, RejectedValue> {
     dispatch: jest.MockedFn<any>;
 
     getState: () => StateSchema;
 
-    actionCreator: (arg: Arg) => AsyncThunkAction<Return, Arg, { rejectValue: RejectedValue }>;
+    actionCreator: ActionCreatorType<Return, Arg, RejectedValue>;
 
     api: jest.MockedFunctionDeep<AxiosStatic>;
 
     navigate: jest.MockedFn<any>;
 
-    constructor(
-        actionCreator: (arg: Arg) => AsyncThunkAction<Return, Arg, { rejectValue: RejectedValue }>,
-        state?: DeepPartial<StateSchema>,
-    ) {
+    constructor(actionCreator: ActionCreatorType<Return, Arg, RejectedValue>) {
         this.actionCreator = actionCreator;
         this.dispatch = jest.fn();
-        this.getState = jest.fn(() => state as StateSchema);
+        this.getState = jest.fn();
 
+        this.api = mockedAxios;
         this.navigate = jest.fn();
     }
 
